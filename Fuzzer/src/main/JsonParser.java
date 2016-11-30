e752cd094af5d7ba3521a6336f977bde7e36616f
Pierre-Henri Collin <pierrehenri.collin@gmail.com>
2016-11-30 17:25:31 +0100

    swagger parser

--- Fuzzer/src/main/JsonParser.java
+++ Fuzzer/src/main/JsonParser.java
@@ -3,8 +3,6 @@ package main;
 import io.swagger.parser.SwaggerParser;
 import io.swagger.models.Swagger;
 
-import 
-
 public class JsonParser {
 
 	Swagger swagger = new SwaggerParser().read("http://petstore.swagger.io/v2/swagger.json");
